2397e2f5390fb1c2b3571adf2fec121ab29dc453
Николай <wasilev-n@mail.ru>
2023-11-14 21:12:12 +0500

    0.1

--- src/App.js
+++ src/App.js
@@ -8,8 +8,17 @@ import { Sort } from "./components/Sort";
 import { Categories } from "./components/Categories";
 import { PizzaBlock } from "./components/PizzaBlock";
 
-import pizzas from "./assets/pizzas.json";
 function App() {
+  // хронилище всех пиц
+  const [items, setItems] = React.useState([]);
+  React.useEffect(() => {
+    // запрос на сервер для получения данных
+    fetch("http://localhost:5500/data")
+      .then((res) => res.json())
+      .then((json) => setItems(json));
+  }, []);
+
+  // console.log(items);
   return (
     <div className="wrapper">
       <Header />
@@ -21,7 +30,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((item) => {
+            {items.map((item) => {
               return <PizzaBlock key={item.id + item.title} {...item} />;
             })}
           </div>
